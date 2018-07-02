f0ce1efcddd91d05a27995c523f4206554753407
vasyaa <vasyaa@gmail.com>
2018-07-02 19:05:53 -0400

    Weighted jobs scheduling

--- solvers/weighted_jobs_scheduling.h
+++ solvers/weighted_jobs_scheduling.h
@@ -8,10 +8,16 @@
 #ifndef SOLVERS_WEIGHTED_JOBS_SCHEDULING_H_
 #define SOLVERS_WEIGHTED_JOBS_SCHEDULING_H_
 
+//
+// https://courses.cs.washington.edu/courses/cse521/13wi/slides/06dp-sched.pdf
+// https://ocw.tudelft.nl/wp-content/uploads/Algoritmiek_Weighted_Interval_Scheduling.pdf
+//
+
 #include <vector>
 #include <list>
 #include <algorithm>
 #include <iterator>
+#include <tuple>
 
 #include <iostream>
 
@@ -39,41 +45,81 @@ typedef std::vector<Job> container_type;
 
 class WeightedJbScheduling {
 public:
+    WeightedJbScheduling() {
+        data = std::vector<Job>(1);
+    }
+
     void add(const Job& i) {
         data.push_back(i);
     }
 
-    container_type solve() {
-        container_type rc;
-
-        std::sort(data.begin(), data.end());
-
-        Job cur = data[0];
-        rc.push_back(cur);
-        for(int i = 1; i != (int)data.size(); i++) {
-            if(cur.finish <= data[i].start) {
-                cur = data[i];
-                rc.push_back(cur);
+    void calc_p() {
+        p = std::vector<int>(data.size(), 0);
+        for(int i = data.size() - 1; i >= 0; i-- ) {
+            for(int k = i - 1; k >= 0; k--) {
+                if(data[k].finish <= data[i].start) {
+                    p[i] = k;
+                    break;
+                }
             }
         }
-
-        return rc;
     }
 
-    container_type reqIteration(const container_type& c, int max) {
-        container_type rc;
-        return rc;
+    // recursive solution
+    int recursive_compute_opt(int j) {
+        if(j == 0) {
+            return 0;
+        }
+        else {
+            int rc;
+            rc = std::max(
+                    data[j].weight + recursive_compute_opt(p[j]),
+                    recursive_compute_opt(j - 1));
+            return rc;
+        }
     }
 
-    container_type solve2() {
-        container_type rc;
+    // bottom up solution
+    int iterative_compute_opt() {
+        dp = std::vector<int>(data.size(), 0);
+        for(int j = 0; j < (int)dp.size(); j++) {
+            dp[j] = std::max(data[j].weight + dp[p[j]], dp[p[j - 1]]);
+        }
 
-
-
-        return rc;
+        return dp[dp.size() - 1];
     }
+
+    void recreate_path(int j) {
+        if(j == 0) {
+            return;
+        }
+        if(data[j].weight + dp[p[j]] > dp[j - 1]) {
+            path.push_back(data[j]);
+            recreate_path(p[j - 1]);
+        }
+        else {
+            recreate_path(p[j - 1]);
+        }
+    }
+
+    std::tuple<int, container_type> solve() {
+        std::sort(data.begin(), data.end());
+        calc_p();
+        dp = std::vector<int>(data.size(),0);
+
+//        int res = recursive_compute_opt(data.size() - 1);
+        int res = iterative_compute_opt();
+        path = container_type();
+        recreate_path(data.size() - 1);
+        std::reverse(path.begin(), path.end());
+        return make_tuple(res, path);
+    }
+
 private:
     container_type data;
+    std::vector<int> dp;
+    std::vector<int> p;
+    std::vector<Job> path;
 };
 
 
@@ -89,8 +135,10 @@ inline void test() {
         sch.add(i);
     }
 
-    container_type rc = sch.solve();
-
+    auto res = sch.solve();
+    container_type rc = std::get<1>(res);
+    int val = std::get<0>(res);
+    std::cout << "value=" << val << std::endl;
     std::copy(rc.begin(), rc.end(), std::ostream_iterator<Job>(std::cout, "\n"));
 }
 
